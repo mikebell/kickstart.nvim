931ed73d9debf1a51f4da44c940849777c91d832
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2024-09-11 21:39:02 +0100

    feat: 🎸 keymap to open file explorer

--- lua/kickstart/plugins/nvim-tree.lua
+++ lua/kickstart/plugins/nvim-tree.lua
@@ -1,12 +1,19 @@
 -- Nvim-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-tree/nvim-tree.lua
 
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle File Explorer' })
+
 return {
   'nvim-tree/nvim-tree.lua',
   version = '*',
   lazy = false,
   dependencies = {
     'nvim-tree/nvim-web-devicons',
+  },
+  setup = {
+    update_focused_file = {
+      enable = true,
+    },
   },
   config = function()
     require('nvim-tree').setup {
